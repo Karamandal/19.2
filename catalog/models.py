b54ffdea107d99fb03bb75701d3e49584f5ab6d7
Karamandal <acandreyan@gmail.com>
2024-05-27 00:03:55 +0700

    Откатил миграцию

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 NULLABLE = {"blank": True, "null": True}
 
+
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.CharField(max_length=150, verbose_name='Описание')
